6521f157364607c883d333ad2b82705ef5edcf32
kashwaa <mostafa.kashwaa@gmail.com>
2021-03-17 11:52:12 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Mostafa Kashwaa on 17/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
